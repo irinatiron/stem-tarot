56d187f3094762adff5e17806d1e324c5df9265b
irinatiron <irinatirongiurgiu@gmail.com>
2025-08-20 01:29:48 +0200

    style(CardDetailPage): navigation with buttons for arcana and goddess info, added react-icons library

--- src/pages/CardDetailPage.tsx
+++ src/pages/CardDetailPage.tsx
@@ -3,10 +3,12 @@ import { useParams, useNavigate } from "react-router-dom";
 import { getCardById } from "../services/tarotService";
 import { TarotCard } from "../types/tarot";
 import styles from "./CardDetailPage.module.css"; 
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 export default function CardDetailPage() {
   const { id } = useParams<{ id: string }>();
   const [card, setCard] = useState<TarotCard | null>(null);
+  const [showGoddess, setShowGoddess] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,26 +17,64 @@ export default function CardDetailPage() {
     }
   }, [id]);
 
-  if (!card) return <p>Cargando...</p>;
+  if (!card) return <p className={styles.loadingText}>Cargando...</p>;
 
   return (
-    <div className={`${styles.cardDetailContainer} mainContainer`}>
-      <div className={styles.cardContent}>
-        <div className={styles.cardImageContainer}>
-          <h2>{card.arcaneName}</h2>
-          <img src={card.arcaneImage.imageSrc} alt={card.arcaneName} width="200" />
-          <p>{card.arcaneDescription}</p>
-        </div>
+    <>
+      <h1 className="pageTitle">
+        Detalle de la carta: <span className={styles.arcaneNameTitle}>{card.arcaneName}</span>
+      </h1>
 
-        <div className={styles.cardGoddessContainer}>
-          <h2>Diosa: {card.goddessName}</h2>
-          <img src={card.goddessImage.imageSrc} alt={card.goddessName} width="200" />
-          <p>{card.goddessDescription}</p>
+      <div className={`${styles.cardDetailContainer} mainContainer`}>
+        {/* Contenedor principal con ancho completo */}
+        {!showGoddess ? (
+          <div className={styles.cardFullWidth}>
+            <h2 className={styles.arcaneName}>{card.arcaneName}</h2>
+            <img 
+              className={styles.arcaneImage} 
+              src={card.arcaneImage.imageSrc} 
+              alt={card.arcaneName} 
+              width="300" 
+            />
+            <p className={styles.arcaneDescription}>{card.arcaneDescription}</p>
+            <button 
+              className={styles.nextButton} 
+              onClick={() => setShowGoddess(true)}
+            >
+              <MdKeyboardArrowRight />
+
+            </button>
+          </div>
+        ) : (
+          <div className={styles.cardFullWidth}>
+            
+            <h2 className={styles.goddessName}>Diosa: {card.goddessName}</h2>
+            <img 
+              className={styles.goddessImage} 
+              src={card.goddessImage.imageSrc} 
+              alt={card.goddessName} 
+              width="300" 
+            />
+            <p className={styles.goddessDescription}>{card.goddessDescription}</p>
+            <button 
+              className={styles.previousButton} 
+              onClick={() => setShowGoddess(false)}
+            >
+              <MdKeyboardArrowLeft />
+
+            </button>
+          </div>
+        )}
+
+        <div className={styles.returnContainer}>
+          <button 
+            className={styles.buttonBack} 
+            onClick={() => navigate("/cartas")}
+          >
+            Regresar a las cartas
+          </button>
         </div>
       </div>
-      <div className={styles.returnContainer}>
-        <button className={styles.button} onClick={() => navigate("/cartas")}>Regresar a las cartas</button>
-      </div>
-    </div>
+    </>
   );
 }
