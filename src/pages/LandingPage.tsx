51a1cb34c6f58b5f68b7b3690da1909eff215ab8
irinatiron <irinatirongiurgiu@gmail.com>
2025-08-20 01:36:33 +0200

    style(LandingPage): custom 'Comenzar' button

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import styles from "./LandingPage.module.css"; 
+import styles from "./LandingPage.module.css";
 
 export default function LandingPage() {
     const navigate = useNavigate();
@@ -8,7 +8,13 @@ export default function LandingPage() {
         <div className="mainContainer">
             <div className={styles.logoContainer}>
                 <h1 className={styles.title}>TAROT STEM</h1>
-                <button className={styles.button} onClick={() => navigate("/cartas")}>Comenzar</button>
+                <button className={styles.button} onClick={() => navigate("/cartas")}>
+                    Comenzar
+                    <div className={styles.button__horizontal}></div>
+                    <div className={styles.button__vertical}></div>
+                </button>
+
+                {/* <button className={styles.button} onClick={() => navigate("/cartas")}>Comenzar</button> */}
             </div>
         </div>
     );
