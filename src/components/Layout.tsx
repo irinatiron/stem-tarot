05470e5548f294d10e0177ab1e607427459775c2
irinatiron <irinatirongiurgiu@gmail.com>
2025-08-24 21:20:49 +0200

    update(Vercel)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout() {
   return (
     <div className="layoutGrid">
+      <Analytics />
       <Navbar />
       <Header />
       <main>
